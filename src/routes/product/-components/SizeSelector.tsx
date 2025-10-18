518aab4c5b83cd38edf7a356b0472b85cf1599ac
Nur <nuralam.rsc@gmail.com>
2025-10-18 23:31:35 +0600

    fix(product): make the size selectore interactive

--- src/routes/product/-components/SizeSelector.tsx
+++ src/routes/product/-components/SizeSelector.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
+import { cn } from "../../../lib/utils";
+
 const SizeSelector = () => {
+  const [selectedSize, setSelectedSize] = useState("S");
+
   return (
     <div>
-      <p className="text-c-base font-semibold">Size: S</p>
+      <p className="text-c-base font-semibold">Size: {selectedSize}</p>
       <div className="flex mt-[12px] gap-[8.5px]">
-        <button className="w-[72px] h-[44px] bg-vibrant font-semibold rounded-[12px] text-white">
-          S
-        </button>
-        <button className="w-[72px] h-[44px] font-semibold rounded-[12px] text-body-text border border-gray-450">
-          M
-        </button>
+        {["S", "M", "L", "XL", "2XL"].map((size) => (
+          <button
+            key={`size-${size}`}
+            onClick={() => setSelectedSize(size)}
+            className={cn(
+              "w-[72px] h-[44px] font-semibold rounded-[12px] transition-all active:scale-[0.98] cursor-pointer",
+              selectedSize === size
+                ? "bg-vibrant text-white"
+                : "text-body-text border border-gray-450",
+            )}
+          >
+            {size}
+          </button>
+        ))}
       </div>
     </div>
   );
