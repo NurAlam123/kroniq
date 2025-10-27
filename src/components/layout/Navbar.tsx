85964491f80b6af02a73d3a7def58dcdf2b6b490
Nur <nuralam.rsc@gmail.com>
2025-10-27 19:09:47 +0600

    fix(navbar): fix hamburger menu
    feat(navbar): add login and register button

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
           </div>
         </Link>
         <SearchBar />
-        <ActionButton />
+        <MenuButton />
       </div>
     </div>
   );
@@ -46,26 +46,57 @@ const SearchBar = () => {
 
 const Menu = ({ className }: { className?: string }) => {
   return (
-    <div className={cn("flex items-center gap-6 justify-center", className)}>
-      <button className="cursor-pointer">
-        <Image src="/icons/user.svg" alt="user icon" width={24} height={24} />
+    <div
+      className={cn(
+        "flex max-lg:flex-col items-center gap-6 justify-center",
+        className,
+      )}
+    >
+      <div className="space-y-2">
+        <Link
+          to="/"
+          className="bg-blue-500 px-6 py-2 rounded-full text-white transition-all ease-in-out font-medium text-center block shadow-sm active:scale-95 hover:bg-blue-600 lg:hidden"
+        >
+          Register
+        </Link>
+
+        <Link
+          to="/"
+          className="block bg-white lg:bg-blue-500 px-6 py-2 rounded-full text-blue-500 lg:text-white transition-all ease-in-out font-medium text-center shadow-sm active:scale-95 hover:bg-gray-50 lg:hover:bg-blue-600"
+        >
+          Login
+        </Link>
+      </div>
+
+      <button className="cursor-pointer hidden max-lg:items-center max-lg:gap-2">
+        <Image
+          src="/icons/user.svg"
+          alt="user icon"
+          width={24}
+          height={24}
+          className="size-5 lg:size-6"
+        />
+        <span className="lg:hidden text-body-text">User</span>
       </button>
-      <button
-        className="relative cursor-pointer"
-        onClick={() => console.log("HOHO")}
-      >
-        <Image src="/icons/cart.svg" alt="cart icon" width={24} height={24} />
-        <span className="absolute -top-2 -right-2 w-5 h-5 bg-blue-500 rounded-full text-white text-xs flex items-center justify-center font-medium">
-          3
-        </span>
-      </button>
+
+      <CartButton className="max-lg:hidden" />
+
+      <div className="hidden">
+        <Link
+          to="/"
+          className="block w-full bg-red-500 px-6 py-2 rounded-full text-white transition-all ease-in-out font-medium text-center shadow-sm active:scale-95 hover:bg-red-600"
+        >
+          Logout
+        </Link>
+      </div>
     </div>
   );
 };
 
-const ActionButton = () => {
+const MenuButton = () => {
   return (
-    <div>
+    <div className="max-lg:flex max-lg:justify-center max-lg:gap-4">
+      <CartButton className="lg:hidden" />
       <div className="relative">
         <button popoverTarget="menu" className="lg:hidden">
           <Image src="/icons/hamburger-menu.svg" className="size-9" />
@@ -84,6 +115,34 @@ const ActionButton = () => {
         <Menu />
       </div>
     </div>
+  );
+};
+
+const CartButton = ({
+  className,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return (
+    <button
+      className={cn(
+        "relative cursor-pointer max-lg:flex max-lg:items-center max-lg:gap-2",
+        className,
+      )}
+      {...props}
+    >
+      <div className="relative">
+        <Image
+          src="/icons/cart.svg"
+          alt="cart icon"
+          width={24}
+          height={24}
+          className="size-5 lg:size-6"
+        />
+        <span className="absolute -top-3 -right-3 lg:-top-2 lg:-right-2 w-5 h-5 bg-blue-500 rounded-full text-white text-xs flex items-center justify-center font-medium">
+          3
+        </span>
+      </div>
+    </button>
   );
 };
 
